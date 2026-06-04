942e96678e4a9d59831903b4c04c56215434c725
AvinashRajput12 <rajputboy945456@gmail.com>
2026-06-05 00:34:50 +0530

    deploy setup

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,7 @@ function Navbar({ cartItems, setShowCart }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
       <div className="container">
+
         <a
           className="navbar-brand"
           href="#home"
@@ -10,46 +11,65 @@ function Navbar({ cartItems, setShowCart }) {
           Hotel Royal
         </a>
 
-        <div className="navbar-nav ms-auto d-flex align-items-center">
-          <a
-            className="nav-link"
-            href="#home"
-            onClick={() => setShowCart(false)}
-          >
-            Home
-          </a>
+        {/* Mobile Menu Button */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarContent"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-          <a
-            className="nav-link"
-            href="#services"
-            onClick={() => setShowCart(false)}
-          >
-            Services
-          </a>
+        {/* Navbar Links */}
+        <div
+          className="collapse navbar-collapse"
+          id="navbarContent"
+        >
+          <div className="navbar-nav ms-auto align-items-lg-center">
 
-          <a
-            className="nav-link"
-            href="#menu"
-            onClick={() => setShowCart(false)}
-          >
-            Menu
-          </a>
+            <a
+              className="nav-link"
+              href="#home"
+              onClick={() => setShowCart(false)}
+            >
+              Home
+            </a>
 
-          <a
-            className="nav-link"
-            href="#contact"
-            onClick={() => setShowCart(false)}
-          >
-            Contact
-          </a>
+            <a
+              className="nav-link"
+              href="#services"
+              onClick={() => setShowCart(false)}
+            >
+              Services
+            </a>
 
-          <button
-            className="btn btn-warning ms-3"
-            onClick={() => setShowCart(true)}
-          >
-            🛒 Cart ({cartItems.length})
-          </button>
+            <a
+              className="nav-link"
+              href="#menu"
+              onClick={() => setShowCart(false)}
+            >
+              Menu
+            </a>
+
+            <a
+              className="nav-link"
+              href="#contact"
+              onClick={() => setShowCart(false)}
+            >
+              Contact
+            </a>
+
+            <button
+              className="btn btn-warning ms-lg-3 mt-2 mt-lg-0"
+              onClick={() => setShowCart(true)}
+            >
+              🛒 Cart ({cartItems.length})
+            </button>
+
+          </div>
         </div>
+
       </div>
     </nav>
   );
